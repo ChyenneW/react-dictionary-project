f1ce6e94720956c5563a5d8f1bb183537768796e
ChyenneW <williamscheyenne771@gmail.com>
2022-04-25 00:58:22 -0500

    Update Definition.js

--- src/Definition.js
+++ src/Definition.js
@@ -6,10 +6,21 @@ export default function Definition(props) {
   return (
     <div>
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>
+
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index} className="definitions">
+            <p>
+              {index + 1}. {definition.definition}
+            </p>
+          </div>
+        );
+      })}
+      <p className="examples">
         <em>{props.meaning.definitions[0].example}</em>
       </p>
     </div>
   );
 }
+
+// documentation: <p>{props.meaning.definitions[0].definition}</p>
