f912c835339770ea5dbd057bc75782e6a52e136b
ChyenneW <williamscheyenne771@gmail.com>
2022-04-27 13:20:20 -0500

    fixed bugs

--- src/Definition.js
+++ src/Definition.js
@@ -7,33 +7,30 @@ export default function Definition(props) {
 
   return (
     <div>
-      <h3 className="partOfSpeech">{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div>
+      <div>
+        <h3 className="partOfSpeech">{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
             <div key={index} className="definitions">
               <p>
                 {index + 1}. {definition.definition}
               </p>
             </div>
-            <div>
-              {props.meaning.definitions.map(function (example, index) {
-                return (
-                  <div className="examples" key={index}>
-                    <p>
-                      <em>{example.example}</em>
-                    </p>
-                  </div>
-                );
-              })}
+          );
+        })}
+      </div>
+      <div>
+        <h3 className="examplesHeading">Examples</h3>
+        {props.meaning.definitions.map(function (example, index) {
+          return (
+            <div className="examples" key={index}>
+              <p>
+                <em>{example.example}</em>
+              </p>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-//  <h3 className="examplesHeading">Examples</h3>;
-
-//
